fd8a8071e79eb890253359c41900d15fcffa142d
Expant <anton532202@gmail.com>
2024-02-18 22:08:58 +0300

    add ui text

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    title: 'RSS агрегатор',
+    subtitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    rssForm: {
+      label: 'Cсылка RSS',
+      button: 'Добавить',
+    },
+    example: 'Пример: https://lorem-rss.hexlet.app/feed',
+    feedbacks: {
+      success: 'RSS успешно загружен',
+      invalid: 'Ссылка должна быть валидным URL',
+      exist: 'RSS уже существует',
+    }
+  }
+}
