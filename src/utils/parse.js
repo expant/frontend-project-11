625a27dbf60a22da10033dbffdd0384ec341a43a
Expant <anton532202@gmail.com>
2024-02-19 22:58:49 +0300

    realize parser

---
+++ src/utils/parse.js
@@ -0,0 +1,29 @@
+const getElements = (doc) => ({
+  feedElement: {
+    title: doc.querySelector('channel > title'),
+    description: doc.querySelector('channel > description'),
+  },
+  postsElement: doc.querySelectorAll('item'),
+});
+
+export default (string) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(string, 'application/xhtml+xml');
+  const { feedElement, postsElement } = getElements(doc);
+
+  const feed = {
+    title: feedElement.title.textContent,
+    description: feedElement.description.textContent,
+  };
+  const posts = Array.from(postsElement).map((postElement) => {
+    const titleElement = postElement.querySelector('title');
+    const descriptionElement = postElement.querySelector('description');
+    const linkElement = postElement.querySelector('link');
+    const title = titleElement.textContent;
+    const description = descriptionElement.textContent;
+    const link = linkElement.textContent;
+    return { title, description, link };
+  });
+  
+  return { feed, posts };
+};
