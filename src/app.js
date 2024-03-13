abbc13bf7bea7f4f07848e6f3dadd9316ca737ae
Expant <anton532202@gmail.com>
2024-03-13 15:24:19 +0300

    refactor: rss-form validation

--- src/app.js
+++ src/app.js
@@ -1,186 +1,247 @@
 import axios from 'axios';
+import i18next from 'i18next';
+import * as yup from 'yup';
 import { isEmpty } from 'lodash';
+import resources from './locales/index.js';
 import watch from './view.js';
 import parse from './utils/parse.js';
 
 const UPDATE_INTERVAL = 5000;
 const TIMEOUT = 10000;
-
-const makeRequest = (url) => axios.get(
-  `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`,
-  { timeout: TIMEOUT },
-);
-
-const setIdOfTheUpdatedData = (data, state, feedId) => {
-  const { post } = { ...data };
-  const { posts: postsFromState } = { ...state.lists };
-  const lastPost = postsFromState[0];
-  // console.log(post);
-  return {
-    ...post,
-    id: lastPost.id + 1,
-    feedId,
-  };
+const STATUS = {
+  FILLING: 'filling',
+  SENDING: 'sending',
+  SUCCESS: 'success',
+  FAIL: 'fail',
 };
 
-const setId = (data, state) => {
-  const { feed, posts } = { ...data };
-  if (isEmpty(state.lists.feeds) && isEmpty(state.lists.posts)) {
-    const feedWithId = { ...feed, id: 0 };
-    const postsWithId = posts.map((post, i) => ({
-      ...post,
-      id: i,
-      feedId: feedWithId.id,
-    }));
-    return { feed: feedWithId, posts: postsWithId };
-  }
+// const makeRequest = (url) => axios.get(
+//   `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`,
+//   { timeout: TIMEOUT },
+// );
 
-  const {
-    feeds: feedsFromState,
-    posts: postsFromState,
-  } = { ...state.lists };
+// const setIdOfTheUpdatedData = (data, state, feedId) => {
+//   const { post } = { ...data };
+//   const { posts: postsFromState } = { ...state.lists };
+//   const lastPost = postsFromState[0];
+//   // console.log(post);
+//   return {
+//     ...post,
+//     id: lastPost.id + 1,
+//     feedId,
+//   };
+// };
 
-  const lastFeed = feedsFromState[feedsFromState.length - 1];
-  // const lastPost = postsFromState[postsFromState.length - 1];
-  const lastPost = postsFromState[0];
-  const feedWithId = { ...feed, id: lastFeed.id + 1 };
-  const postsWithId = posts.map((post, i) => ({
-    ...post,
-    id: lastPost.id + (i + 1),
-    feedId: feedWithId.id,
-  }));
-  return { feed: feedWithId, posts: postsWithId };
-};
+// const setId = (data, state) => {
+//   const { feed, posts } = { ...data };
+//   if (isEmpty(state.lists.feeds) && isEmpty(state.lists.posts)) {
+//     const feedWithId = { ...feed, id: 0 };
+//     const postsWithId = posts.map((post, i) => ({
+//       ...post,
+//       id: i,
+//       feedId: feedWithId.id,
+//     }));
+//     return { feed: feedWithId, posts: postsWithId };
+//   }
 
-export default (elements, i18n, initialState) => {
-  // View
-  const watchedState = watch(elements, i18n, initialState);
+//   const {
+//     feeds: feedsFromState,
+//     posts: postsFromState,
+//   } = { ...state.lists };
 
-  // Controller
-  const handleError = (name, key) => {
-    watchedState.status = 'invalid';
-    watchedState.error = { [name]: key };
-  };
+//   const lastFeed = feedsFromState[feedsFromState.length - 1];
+//   // const lastPost = postsFromState[postsFromState.length - 1];
+//   const lastPost = postsFromState[0];
+//   const feedWithId = { ...feed, id: lastFeed.id + 1 };
+//   const postsWithId = posts.map((post, i) => ({
+//     ...post,
+//     id: lastPost.id + (i + 1),
+//     feedId: feedWithId.id,
+//   }));
+//   return { feed: feedWithId, posts: postsWithId };
+// };
 
-  const handleResponse = (res, urlObj) => {
-    const { url, contentLength, feedId } = urlObj;
-    const { content_length: newContentLength } = res.data.status;
-    if (contentLength === newContentLength) {
-      return;
-    }
+// const validateRSSForm = () => {
 
-    const parsedData = parse(res.data.contents);
-    const newPost = parsedData.posts.find((post) => {
-      const currentPost = watchedState.lists.posts.find(
-        (postFromState) => postFromState.title === post.title,
-      );
+// };
 
-      return !currentPost;
+const getElements = () => ({
+  init: {
+    title: document.querySelector('h1'),
+    subtitle: document.querySelector('p.lead'),
+    rssForm: {
+      form: document.querySelector('.rss-form'),
+      field: document.querySelector('#url-input'),
+      label: document.querySelector('.form-label'),
+      button: document.querySelector('.rss-form .btn'),
+    },
+    example: document.querySelector('.example-muted'),
+    modal: {
+      readCompletely: document.querySelector('.modal-footer .btn-primary'),
+      close: document.querySelector('.modal-footer .btn-secondary'),
+    },
+  },
+  modal: {
+    title: document.querySelector('.modal-title'),
+    description: document.querySelector('.modal-body'),
+    readCompletely: document.querySelector('.modal-footer .btn-primary'),
+  },
+  feedback: document.querySelector('.feedback'),
+  posts: {
+    parent: document.querySelector('.posts'),
+    title: document.querySelector('.posts h2'),
+    list: document.querySelector('.posts ul'),
+  },
+  feeds: {
+    parent: document.querySelector('.feeds'),
+    title: document.querySelector('.feeds h2'),
+    list: document.querySelector('.feeds ul'),
+  },
+});
+
+const validateForm = (url, watchedState, schema) => {
+  schema.validate({ url }, { abortEarly: false })
+    .then(() => {
+      watchedState.rssForm = {
+        error: '',
+        isValid: true,
+      };
+    })
+    .catch((err) => {
+      // const name = err.inner[0].path;
+      const [key] = err.errors;
+      watchedState.rssForm = {
+        error: key,
+        isValid: false,
+      };
     });
+}
 
-    const data = { feed: parsedData.feed, post: newPost };
-
-    const post = setIdOfTheUpdatedData(data, watchedState, feedId);
-    const otherUrls = watchedState.urls.filter((urlItem) => urlItem.feedId !== feedId);
-    const newUrl = { url, contentLength: newContentLength, feedId };
-
-    watchedState.urls = [...otherUrls, newUrl];
-    watchedState.lists.posts = [post, ...watchedState.lists.posts];
-    watchedState.status = 'updated';
-  };
-
-  const handleModal = () => {
-    const { list } = elements.posts;
-    const readPostsElements = list.querySelectorAll('li');
-    const updateStateOfReadPosts = (event) => {
-      if (!event.target.dataset.id) {
-        return;
-      }
-      const postId = parseInt(event.target.dataset.id, 10);
-      const currentPost = watchedState.lists.posts.find((post) => post.id === postId);
-      const otherPosts = watchedState.uiState.readPosts.filter((post) => post.id !== postId);
-      watchedState.uiState.readPosts = [...otherPosts, currentPost];
-    };
-
-    readPostsElements.forEach((readPostEl) => {
-      readPostEl.addEventListener('click', (e) => updateStateOfReadPosts(e));
-    });
-  };
-
-  const updatePosts = () => watchedState.urls.forEach((urlObj) => {
-    makeRequest(urlObj.url)
-      .then((res) => handleResponse(res, urlObj))
-      .then(() => handleModal())
-      .catch(() => handleError('networkError', 'feedbacks.networkError'));
-  });
-
-  const watchPosts = () => {
-    if (watchedState.urls.length === 0) {
-      return setTimeout(watchPosts, UPDATE_INTERVAL);
-    }
-    updatePosts();
-    return setTimeout(watchPosts, UPDATE_INTERVAL);
-  };
-  watchPosts();
-
+const handleRSSForm = (elements, watchedState, schema) => {
   const { form } = elements.init.rssForm;
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
-    
-    
-    // watchedState.status = 'sending';
-    schema.validate({ url }, { abortEarly: false })
-      // sending a request
-      .then(() => {
-        const existingUrl = watchedState.urls.find((currentUrl) => currentUrl.url === url);
-        if (existingUrl) {
-          watchedState.error = { exist: 'feedbacks.exist' };
-          watchedState.status = 'invalid';
-          return;
-        }
+  
+    validateForm(url, watchedState, schema);
+    if (!watchedState.rssForm.isValid) {
+      return;
+    }
 
-        watchedState.error = {};
-        watchedState.status = 'valid';
-        return makeRequest(url);
-      })
-      .catch((err) => {
-        if (err.name && err.name === 'AxiosError') {
-          handleError('networkError', 'feedbacks.networkError');
-          return;
-        }
-        if (!err.inner) return;
-        const name = err.inner[0].path;
-        const [key] = err.errors;
-        handleError(name, key);
-      })
-      // Parsing
-      .then((res) => {
-        if (!res) return;
-        const parsedData = parse(res.data.contents);
-        if (isEmpty(parsedData)) {
-          watchedState.status = 'invalid';
-          watchedState.error = { invalidRSS: 'feedbacks.invalidRSS' };
-          return;
-        }
-
-        const { feed, posts } = setId(parsedData, watchedState);
-        const { content_length: contentLength } = res.data.status;
-        watchedState.urls.push({ url, contentLength, feedId: feed.id });
-        watchedState.lists.feeds = [feed, ...watchedState.lists.feeds];
-        watchedState.lists.posts = [...posts.reverse(), ...watchedState.lists.posts];
-        watchedState.status = 'finished';
-      })
-      .catch(() => handleError('unknownError', 'feedbacks.unknownError'))
-      .then(() => {
-        const { list } = elements.posts;
-        if (list.childNodes.length === 0) {
-          return;
-        }
-        handleModal();
-      });
   });
+}
+
+export default () => {
+  const elements = getElements();
+
+  yup.setLocale({
+    string: {
+      url: 'feedbacks.invalid',
+    },
+  });
+  
+  const schema = yup.object({
+    url: yup.string().required().url(),
+  });
+
+  const i18n = i18next.createInstance();
+  i18n.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  });
+
+   // Model
+   const initialState = {
+    rssForm: {
+      error: '',
+      isValid: null,
+    },
+    UrlSubmissionProcess: {
+      error: '',
+      status: STATUS.FILLING,
+    },
+    feeds: [],
+    posts: [],
+    ui: {
+      seenPosts: [],
+    },
+  };
+
+  // View
+  const watchedState = watch(elements, i18n, initialState);
+
+  // Controller
+  handleRSSForm(elements, watchedState, schema);
+
+  // const handleError = (name, key) => {
+  //   watchedState.status = 'invalid';
+  //   watchedState.error = { [name]: key };
+  // };
+
+  // const handleResponse = (res, urlObj) => {
+  //   const { url, contentLength, feedId } = urlObj;
+  //   const { content_length: newContentLength } = res.data.status;
+  //   if (contentLength === newContentLength) {
+  //     return;
+  //   }
+
+  //   const parsedData = parse(res.data.contents);
+  //   const newPost = parsedData.posts.find((post) => {
+  //     const currentPost = watchedState.lists.posts.find(
+  //       (postFromState) => postFromState.title === post.title,
+  //     );
+
+  //     return !currentPost;
+  //   });
+
+  //   const data = { feed: parsedData.feed, post: newPost };
+
+  //   const post = setIdOfTheUpdatedData(data, watchedState, feedId);
+  //   const otherUrls = watchedState.urls.filter((urlItem) => urlItem.feedId !== feedId);
+  //   const newUrl = { url, contentLength: newContentLength, feedId };
+
+  //   watchedState.urls = [...otherUrls, newUrl];
+  //   watchedState.lists.posts = [post, ...watchedState.lists.posts];
+  //   watchedState.status = 'updated';
+  // };
+
+  // const handleModal = () => {
+  //   const { list } = elements.posts;
+  //   const readPostsElements = list.querySelectorAll('li');
+  //   const updateStateOfReadPosts = (event) => {
+  //     if (!event.target.dataset.id) {
+  //       return;
+  //     }
+  //     const postId = parseInt(event.target.dataset.id, 10);
+  //     const currentPost = watchedState.lists.posts.find((post) => post.id === postId);
+  //     const otherPosts = watchedState.uiState.readPosts.filter((post) => post.id !== postId);
+  //     watchedState.uiState.readPosts = [...otherPosts, currentPost];
+  //   };
+
+  //   readPostsElements.forEach((readPostEl) => {
+  //     readPostEl.addEventListener('click', (e) => updateStateOfReadPosts(e));
+  //   });
+  // };
+
+  // const updatePosts = () => watchedState.urls.forEach((urlObj) => {
+  //   makeRequest(urlObj.url)
+  //     .then((res) => handleResponse(res, urlObj))
+  //     .then(() => handleModal())
+  //     .catch(() => handleError('networkError', 'feedbacks.networkError'));
+  // });
+
+  // const watchPosts = () => {
+  //   if (watchedState.urls.length === 0) {
+  //     return setTimeout(watchPosts, UPDATE_INTERVAL);
+  //   }
+  //   updatePosts();
+  //   return setTimeout(watchPosts, UPDATE_INTERVAL);
+  // };
+  // watchPosts();
+
+
 };
 
 // --------- URL-адреса для тестирования различных вариантов: (ошибки и тд)
@@ -196,3 +257,58 @@ export default (elements, i18n, initialState) => {
 
 // Updates:
 // https://lorem-rss.hexlet.app/feed?unit=second&interval=10
+
+
+
+
+
+// watchedState.status = 'sending';
+    // schema.validate({ url }, { abortEarly: false })
+    //   // sending a request
+    //   .then(() => {
+    //     const existingUrl = watchedState.urls.find((currentUrl) => currentUrl.url === url);
+    //     if (existingUrl) {
+    //       watchedState.error = { exist: 'feedbacks.exist' };
+    //       watchedState.status = 'invalid';
+    //       return;
+    //     }
+
+    //     watchedState.error = {};
+    //     watchedState.status = 'valid';
+    //     return makeRequest(url);
+    //   })
+    //   .catch((err) => {
+    //     if (err.name && err.name === 'AxiosError') {
+    //       handleError('networkError', 'feedbacks.networkError');
+    //       return;
+    //     }
+    //     if (!err.inner) return;
+    //     const name = err.inner[0].path;
+    //     const [key] = err.errors;
+    //     handleError(name, key);
+    //   })
+    //   // Parsing
+    //   .then((res) => {
+    //     if (!res) return;
+    //     const parsedData = parse(res.data.contents);
+    //     if (isEmpty(parsedData)) {
+    //       watchedState.status = 'invalid';
+    //       watchedState.error = { invalidRSS: 'feedbacks.invalidRSS' };
+    //       return;
+    //     }
+
+    //     const { feed, posts } = setId(parsedData, watchedState);
+    //     const { content_length: contentLength } = res.data.status;
+    //     watchedState.urls.push({ url, contentLength, feedId: feed.id });
+    //     watchedState.lists.feeds = [feed, ...watchedState.lists.feeds];
+    //     watchedState.lists.posts = [...posts.reverse(), ...watchedState.lists.posts];
+    //     watchedState.status = 'finished';
+    //   })
+    //   .catch(() => handleError('unknownError', 'feedbacks.unknownError'))
+    //   .then(() => {
+    //     const { list } = elements.posts;
+    //     if (list.childNodes.length === 0) {
+    //       return;
+    //     }
+    //     handleModal();
+    //   });
--- src/view.js
+++ src/view.js
@@ -11,134 +11,146 @@ const renderInitText = (elements, t) => {
   elements.modal.close.textContent = t('modal.close');
 };
 
+// const renderError = (elements, t, error) => {
+//   if (isEmpty(error)) {
+//     return;
+//   }
+//   const { feedback } = elements;
+//   const { field } = elements.init.rssForm;
+//   const [, pathToFeedbackText] = Object.entries(error).flatMap((err) => err);
+
+//   field.classList.add('is-invalid');
+//   feedback.classList.remove('text-success');
+//   feedback.classList.add('text-danger');
+//   feedback.textContent = t(pathToFeedbackText);
+// };
+
+// const renderFeed = (elements, t, watchedState) => {
+//   const { feeds } = elements;
+//   const feedsListElement = feeds.list;
+//   const firstFeed = watchedState.lists.feeds[0];
+//   const { title, description } = firstFeed;
+//   const feedElement = document.createElement('li');
+//   const titleElement = document.createElement('h3');
+//   const descriptionElement = document.createElement('p');
+
+//   feeds.title.textContent = t('feeds');
+//   feedElement.classList.add('list-group-item', 'border-0', 'border-end-0');
+//   titleElement.classList.add('h6', 'm-0');
+//   descriptionElement.classList.add('m-0', 'small', 'text-black-50');
+//   titleElement.textContent = title;
+//   descriptionElement.textContent = description;
+//   feedElement.append(titleElement);
+//   feedElement.append(descriptionElement);
+//   feedsListElement.prepend(feedElement);
+// };
+
+// const renderPosts = (args) => {
+//   const {
+//     elements, t, postsState, readPostsState,
+//   } = args;
+//   const { posts } = elements;
+//   posts.title.textContent = t('posts');
+//   const postsListElement = posts.list;
+
+//   postsListElement.innerHTML = '';
+//   postsState.forEach((post) => {
+//     const { id, title, link } = post;
+//     const postElement = document.createElement('li');
+//     const titleElement = document.createElement('a');
+//     const button = document.createElement('button');
+//     const currentReadPost = readPostsState.find((readPost) => readPost.id === id);
+
+//     postElement.classList.add(
+//       'list-group-item',
+//       'd-flex',
+//       'justify-content-between',
+//       'align-items-start',
+//       'border-0',
+//       'border-end-0',
+//     );
+
+//     if (currentReadPost) {
+//       titleElement.classList.add('fw-normal', 'link-secondary');
+//     } else {
+//       titleElement.classList.add('fw-bold');
+//     }
+//     button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+//     titleElement.setAttribute('href', link);
+//     titleElement.setAttribute('data-id', id);
+//     titleElement.setAttribute('target', '_blank');
+//     titleElement.setAttribute('rel', 'noopener noreferrer');
+//     button.setAttribute('data-id', id);
+//     button.setAttribute('data-bs-target', '#modal');
+//     button.setAttribute('data-bs-toggle', 'modal');
+//     titleElement.textContent = title;
+//     button.textContent = 'Просмотр';
+//     postElement.append(titleElement);
+//     postElement.append(button);
+//     postsListElement.append(postElement);
+//   });
+// };
+
+// const handleReadPosts = (elements, watchedState) => {
+//   const {
+//     title: modalElTitle,
+//     description: modalElDesc,
+//     readCompletely,
+//   } = elements.modal;
+//   const { list } = elements.posts;
+//   const postElements = Array.from(list.querySelectorAll('li'));
+//   const post = watchedState.uiState.readPosts[
+//     watchedState.uiState.readPosts.length - 1
+//   ];
+//   const {
+//     title, description, link, id,
+//   } = post;
+//   const readPostElement = postElements.find((postEl) => {
+//     const btnEl = postEl.querySelector('button');
+//     return parseInt(btnEl.dataset.id, 10) === id;
+//   });
+//   const linkEl = readPostElement.querySelector('a');
+
+//   linkEl.classList.remove('fw-bold');
+//   linkEl.classList.add('fw-normal', 'link-secondary');
+//   modalElTitle.textContent = title;
+//   modalElDesc.textContent = description;
+//   readCompletely.setAttribute('href', link);
+// };
+
+// const handleSendingStatus = (feedback, field, button) => {
+//   field.classList.remove('is-invalid');
+//   feedback.classList.remove('text-danger');
+//   button.setAttribute('disabled', '');
+//   field.setAttribute('readonly', '');
+//   feedback.textContent = '';
+// };
+
+// const handleFinishedStatus = (feedback, field, button, t) => {
+//   feedback.textContent = t('feedbacks.success');
+//   feedback.classList.add('text-success');
+//   field.removeAttribute('readonly');
+//   button.removeAttribute('disabled');
+//   field.classList.remove('is-invalid');
+//   field.focus();
+//   field.value = '';
+// };
+
+// const handleInvalidStatus = (field, button) => {
+//   field.removeAttribute('readonly');
+//   button.removeAttribute('disabled');
+// };
+
 const renderError = (elements, t, error) => {
-  if (isEmpty(error)) {
-    return;
-  }
   const { feedback } = elements;
   const { field } = elements.init.rssForm;
-  const [, pathToFeedbackText] = Object.entries(error).flatMap((err) => err);
 
-  field.classList.add('is-invalid');
-  feedback.classList.remove('text-success');
+  if (feedback.classList.contains('text-success')) {
+    feedback.classList.remove('text-success');
+  }
+  field.classList.add('is-invalid');      
   feedback.classList.add('text-danger');
-  feedback.textContent = t(pathToFeedbackText);
-};
-
-const renderFeed = (elements, t, watchedState) => {
-  const { feeds } = elements;
-  const feedsListElement = feeds.list;
-  const firstFeed = watchedState.lists.feeds[0];
-  const { title, description } = firstFeed;
-  const feedElement = document.createElement('li');
-  const titleElement = document.createElement('h3');
-  const descriptionElement = document.createElement('p');
-
-  feeds.title.textContent = t('feeds');
-  feedElement.classList.add('list-group-item', 'border-0', 'border-end-0');
-  titleElement.classList.add('h6', 'm-0');
-  descriptionElement.classList.add('m-0', 'small', 'text-black-50');
-  titleElement.textContent = title;
-  descriptionElement.textContent = description;
-  feedElement.append(titleElement);
-  feedElement.append(descriptionElement);
-  feedsListElement.prepend(feedElement);
-};
-
-const renderPosts = (args) => {
-  const {
-    elements, t, postsState, readPostsState,
-  } = args;
-  const { posts } = elements;
-  posts.title.textContent = t('posts');
-  const postsListElement = posts.list;
-
-  postsListElement.innerHTML = '';
-  postsState.forEach((post) => {
-    const { id, title, link } = post;
-    const postElement = document.createElement('li');
-    const titleElement = document.createElement('a');
-    const button = document.createElement('button');
-    const currentReadPost = readPostsState.find((readPost) => readPost.id === id);
-
-    postElement.classList.add(
-      'list-group-item',
-      'd-flex',
-      'justify-content-between',
-      'align-items-start',
-      'border-0',
-      'border-end-0',
-    );
-
-    if (currentReadPost) {
-      titleElement.classList.add('fw-normal', 'link-secondary');
-    } else {
-      titleElement.classList.add('fw-bold');
-    }
-    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-    titleElement.setAttribute('href', link);
-    titleElement.setAttribute('data-id', id);
-    titleElement.setAttribute('target', '_blank');
-    titleElement.setAttribute('rel', 'noopener noreferrer');
-    button.setAttribute('data-id', id);
-    button.setAttribute('data-bs-target', '#modal');
-    button.setAttribute('data-bs-toggle', 'modal');
-    titleElement.textContent = title;
-    button.textContent = 'Просмотр';
-    postElement.append(titleElement);
-    postElement.append(button);
-    postsListElement.append(postElement);
-  });
-};
-
-const handleReadPosts = (elements, watchedState) => {
-  const {
-    title: modalElTitle,
-    description: modalElDesc,
-    readCompletely,
-  } = elements.modal;
-  const { list } = elements.posts;
-  const postElements = Array.from(list.querySelectorAll('li'));
-  const post = watchedState.uiState.readPosts[
-    watchedState.uiState.readPosts.length - 1
-  ];
-  const {
-    title, description, link, id,
-  } = post;
-  const readPostElement = postElements.find((postEl) => {
-    const btnEl = postEl.querySelector('button');
-    return parseInt(btnEl.dataset.id, 10) === id;
-  });
-  const linkEl = readPostElement.querySelector('a');
-
-  linkEl.classList.remove('fw-bold');
-  linkEl.classList.add('fw-normal', 'link-secondary');
-  modalElTitle.textContent = title;
-  modalElDesc.textContent = description;
-  readCompletely.setAttribute('href', link);
-};
-
-const handleSendingStatus = (feedback, field, button) => {
-  field.classList.remove('is-invalid');
-  feedback.classList.remove('text-danger');
-  button.setAttribute('disabled', '');
-  field.setAttribute('readonly', '');
-  feedback.textContent = '';
-};
-
-const handleFinishedStatus = (feedback, field, button, t) => {
-  feedback.textContent = t('feedbacks.success');
-  feedback.classList.add('text-success');
-  field.removeAttribute('readonly');
-  button.removeAttribute('disabled');
-  field.classList.remove('is-invalid');
-  field.focus();
-  field.value = '';
-};
-
-const handleInvalidStatus = (field, button) => {
-  field.removeAttribute('readonly');
-  button.removeAttribute('disabled');
+  feedback.textContent = t(error);
 };
 
 export default (elements, i18n, initialState) => {
@@ -146,50 +158,60 @@ export default (elements, i18n, initialState) => {
   renderInitText(elements.init, t);
 
   const watchedState = onChange(initialState, (path, value) => {
-    switch (path) {
-      case 'error': {
-        renderError(elements, t, value);
-        return;
-      }
-      case 'lists.posts': {
-        renderPosts({
-          elements,
-          t,
-          postsState: watchedState.lists.posts,
-          readPostsState: watchedState.uiState.readPosts,
-        });
-        return;
-      }
-      case 'lists.feeds': {
-        renderFeed(elements, t, watchedState);
-        return;
-      }
-      case 'uiState.readPosts': {
-        handleReadPosts(elements, watchedState);
-        return;
-      }
-      // default: console.log(`Unknown path ${path}: ${value}`);
-      default: console.log('');
-    }
+    if (path === 'rssForm') {
+      if (value.isValid) {
+        console.log('Круто');
+      } 
 
-    const { feedback } = elements;
-    const { field, button } = elements.init.rssForm;
-    switch (watchedState.status) {
-      case 'sending': {
-        handleSendingStatus(feedback, field, button);
-        break;
+      if (!value.isValid) {
+        renderError(elements, t, value.error);
       }
-      case 'finished': {
-        handleFinishedStatus(feedback, field, button, t);
-        break;
-      }
-      case 'invalid': {
-        handleInvalidStatus(field, button);
-        return;
-      }
-      // default: console.log(`Unknown status ${watchedState.status}`);
-      default: console.log('');
     }
   });
   return watchedState;
 };
+
+
+// switch (path) {
+//   case 'error': {
+//     renderError(elements, t, value);
+//     return;
+//   }
+//   case 'lists.posts': {
+//     renderPosts({
+//       elements,
+//       t,
+//       postsState: watchedState.lists.posts,
+//       readPostsState: watchedState.uiState.readPosts,
+//     });
+//     return;
+//   }
+//   case 'lists.feeds': {
+//     renderFeed(elements, t, watchedState);
+//     return;
+//   }
+//   case 'uiState.readPosts': {
+//     handleReadPosts(elements, watchedState);
+//     return;
+//   }
+//   // default: console.log(`Unknown path ${path}: ${value}`);
+//   default: console.log('');
+// }
+
+
+// switch (watchedState.status) {
+//   case 'sending': {
+//     handleSendingStatus(feedback, field, button);
+//     break;
+//   }
+//   case 'finished': {
+//     handleFinishedStatus(feedback, field, button, t);
+//     break;
+//   }
+//   case 'invalid': {
+//     handleInvalidStatus(field, button);
+//     return;
+//   }
+//   // default: console.log(`Unknown status ${watchedState.status}`);
+//   default: console.log('');
+// }
