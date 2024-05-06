faada7732a8ff593435fb393113f1113c51ec861
Expant <anton532202@gmail.com>
2024-05-06 11:09:27 +0300

    refactor: update posts

--- src/utils/watchPosts.js
+++ src/utils/watchPosts.js
@@ -6,25 +6,24 @@ import getRequestArgs from './getRequestArgs.js';
 
 const UPDATE_INTERVAL = 5000;
 
-const updatePosts = (watchedState, url, res) => {
-  const rss = parse(res.data.contents);
-  const feed = watchedState.feeds.find((item) => item.url === url);
-  const existingTitles = watchedState.posts
+const getUpdatedPosts = (state, url, contents) => {
+  const rss = parse(contents);
+  const feed = state.feeds.find((item) => item.url === url);
+  const existingTitles = state.posts
     .filter((post) => post.feedId === feed.id)
     .map((post) => post.title);
   const newPosts = rss.posts.filter(({ title }) => !existingTitles.includes(title));
-
   if (newPosts.length === 0) {
     return;
   }
 
-  const lastPost = watchedState.posts[0];
+  const lastPost = state.posts[0];
   const newPostsWithId = newPosts.map((post, i) => ({
     ...post,
     feedId: feed.id,
     id: lastPost.id + (i + 1),
   }));
-  watchedState.posts = [...newPostsWithId.reverse(), ...watchedState.posts];
+  return [...newPostsWithId.reverse(), ...state.posts];
 };
 
 const watchPosts = (elements, watchedState) => {
@@ -32,10 +31,23 @@ const watchPosts = (elements, watchedState) => {
     return setTimeout(() => watchPosts(elements, watchedState), UPDATE_INTERVAL);
   }
 
+  const updatePosts = (contents, url) => {
+    const posts = getUpdatedPosts(
+      { 
+        feeds: watchedState.feeds,
+        posts: watchedState.posts,
+      }, 
+      url, 
+      contents,
+    );
+    if (!posts) return;
+    watchedState.posts = posts;
+  };
+
   const promises = watchedState.feeds.map(({ url }) => {
     watchedState.updatingProcess = { status: STATUS.SENDING };
     return axios.get(...getRequestArgs(url))
-      .then((res) => updatePosts(watchedState, url, res))
+      .then((res) => updatePosts(res.data.contents, url))
       .catch(() => {});
   });
 
